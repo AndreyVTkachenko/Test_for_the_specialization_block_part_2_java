7ba22965f9ad25cd50c2a527dcf4065961ed7ab7
«Andrew» <>
2023-12-03 13:23:48 +0700

    made minor changes to the project

--- src/view/ToyQueue.java
+++ src/view/ToyQueue.java
@@ -17,11 +17,11 @@ public class ToyQueue {
     public void startApplication() {
         Queue<Toy> toyQueue = new PriorityQueue<>((t1, t2) -> t2.getWeight() - t1.getWeight());
 
-        ToyShop сonstructorToyShop = new ConstructorToyShop();
+        ToyShop constructorToyShop = new ConstructorToyShop();
         ToyShop robotToyShop = new RobotToyShop();
         ToyShop dollToyShop = new DollToyShop();
 
-        toyQueue.add(сonstructorToyShop.createToy(1, 2, "конструктор"));
+        toyQueue.add(constructorToyShop.createToy(1, 2, "конструктор"));
         toyQueue.add(robotToyShop.createToy(2, 2, "робот"));
         toyQueue.add(dollToyShop.createToy(3, 6, "кукла"));
 
@@ -33,7 +33,7 @@ public class ToyQueue {
                 int randomValue = random.nextInt(10) + 1;
                 Toy toy;
                 if (randomValue <= 2) {
-                    toy = сonstructorToyShop.createToy(1, 2, "конструктор");
+                    toy = constructorToyShop.createToy(1, 2, "конструктор");
                 } else if (randomValue <= 4) {
                     toy = robotToyShop.createToy(2, 2, "робот");
                 } else {
